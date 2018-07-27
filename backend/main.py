f7c7077b461a757ddb8a99318e5a5da738ae729d
Filipe <filipepgm@hotmail.com>
2018-07-27 21:10:56 +0200

    backend initial commit

---
+++ backend/main.py
@@ -0,0 +1,80 @@
+from flask import Flask, request
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+
+class User:
+    def __init__(self, username, coordinates, event_ids):
+        self.username = username
+        self.coordinates = coordinates #tuple
+        self.events = event_ids
+
+    
+    def __eq__(self, other):
+        return self.username == other.username
+    
+    
+
+class Event:
+    def __init__(self, id, name, coordinates, dt):
+        self.id = id
+        self.name = name
+        self.coordinates = coordinates #tuple
+        self.datetime = dt #datetime
+    
+    def get_simple_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def get_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.coordinates,
+            "members": self.get_members()
+        }
+
+    def get_members(self):
+        return [ user for user in users if self.id in user.events]
+
+
+events = dict() #key = id, value = event
+users = set()
+
+
+@app.route('/')
+def ep_hello():
+    return app.send_static_file('index.html')
+    #return 'Hello, World!'
+
+@app.route('/upload/', methods=['POST'])
+def ep_upload():
+    input = json.loads(request.data)
+
+    user = User(**input)
+    users.add(user)
+
+    return "OK"  #request.data #echo
+
+
+@app.route('/events/', methods=['GET'])
+def ep_events():
+    output = []
+    for event in events.values():
+        output.append(event.get_simple_dict())
+
+    return json.dumps(output)
+
+
+@app.route('/fetch/', methods=['POST'])
+def ep_fetch():
+    input = json.loads(request.data)
+
+    output = []
+    for ev_id in input:
+        output.append( events[ev_id].get_dict() )
+        
+    return json.dumps(output)
