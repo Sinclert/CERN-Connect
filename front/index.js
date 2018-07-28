0e65bcacb03611978d428873617f4427827dcbaa
Filipe <filipepgm@hotmail.com>
2018-07-28 12:41:38 +0200

    add location to frontend

--- front/index.js
+++ front/index.js
@@ -7,3 +7,39 @@ var mymap = L.map('mapid').setView([46.2363, 6.0412], 16);
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap);
+
+	// ####################################################################
+	// Leaflet style get location:
+	mymap.locate({setView: false, maxZoom: 16}); // setView: true if we want to set the map to the user position
+
+	function onLocationFound(e) {
+		var radius = e.accuracy / 2;
+	
+		L.marker(e.latlng).addTo(mymap)
+			.bindPopup("You are within " + radius + " meters from this point").openPopup();
+	
+		L.circle(e.latlng, radius).addTo(mymap);
+
+		console.log(e.latlng)
+		//TODO send this to server
+		sendLocation(e)
+	}
+	
+	mymap.on('locationfound', onLocationFound);
+
+	// Not working
+	function sendLocation(location) {
+		var xhr = new XMLHttpRequest();
+		var url = "http://127.0.0.1:5000/";
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				var json = JSON.parse(xhr.responseText);
+				console.log(json);
+			}
+		};
+		console.log(location.latlng);
+		var data = JSON.stringify({"username": "myusername", "coordinates": [location.latlng.lat, location.latlng.lng], "events": []  });
+		xhr.send(data);
+	}
